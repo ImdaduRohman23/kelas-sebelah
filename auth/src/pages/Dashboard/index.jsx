ab00611e41bd8ac626126fcc8ad9a9033e91da9c
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-25 17:00:18 +0700

    FD 3 => Belajaran CRUD

--- auth/src/pages/Dashboard/index.jsx
+++ auth/src/pages/Dashboard/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
+
+    const [users, setUsers] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('https://reqres.in/api/users?page=2')
+            .then((res) => setUsers(res.data.data))
+            .catch((err) => console.log(err))
+    }, [])
+
+    console.log(users)
+
     return (
         <div>
             <h1>Dashboard</h1>
+            {
+                users.map((user) => (
+                    <>
+                        <p>{user.first_name}</p>
+                        <p>{user.email}</p>
+                        <img src={user.avatar} alt="" />
+                    </>
+                ))
+            }
         </div>
     )
 }
